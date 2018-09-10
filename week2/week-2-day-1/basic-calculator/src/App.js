b41836e163b2c669ad688d710d7fdd0b436192b6
David DeArmon <david.dearmon@gmail.com>
2018-09-10 15:54:14 -0500

    updated

--- week2/week-2-day-1/basic-calculator/src/App.js
+++ week2/week-2-day-1/basic-calculator/src/App.js
@@ -8,13 +8,10 @@ class App extends Component {
     this.state={
       inputOne:'',
       inputTwo:'',
-      display:'',
       select:'Add'
     }
   }
-  updateDisplay(result){
-    this.setState({display:result})
-  }
+ 
   handleInput(e,state){
     this.setState({[state]:e})
   }
@@ -27,21 +24,22 @@ class App extends Component {
       <div className="App">
         <header><h1>Calculator</h1></header>
         <div className="Entry">
-          <input value={this.state.inputOne} onChange={event=>this.handleInput(event.target.value, "inputOne")} />
-          <input value={this.state.inputTwo} onChange={event=>this.handleInput(event.target.value, "inputTwo")}/>
+          <input placeholder="First Number" value={this.state.inputOne} onChange={event=>this.handleInput(event.target.value, "inputOne")} />
           <select value={this.state.select} onChange={e=>this.handleOption(e)}>
             <option value ="Add">Add</option>
             <option value ="Subtract">Subtract</option>
             <option value ="Multiply">Multiply</option>
             <option value ="Divide">Divide</option>
          </select>
+          <input placeholder="Second Number" value={this.state.inputTwo} onChange={event=>this.handleInput(event.target.value, "inputTwo")}/>
+          
         </div>
         <div className="Boxes">
           <h3>Box One:{this.state.inputOne} </h3>
           <h3>Box Two:{this.state.inputTwo} </h3>
           <Calculate boxOne={this.state.inputOne} boxTwo={this.state.inputTwo} selection={this.state.select} updateDisplay={this.updateDisplay}/>    
-          {console.log(this.state.display)}
-        </div>   
+        </div>
+        <footer>Made by: David DeArmon</footer>
       </div>
 
     );
